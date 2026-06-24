93b0c2fb3dd88d1d83e5641744671a7411b4cbbf
jalvareziconmm-ui <jalvareziconmm@gmail.com>
2026-06-24 14:56:49 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+console.log("config.js cargado correctamente");
+
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbyzfU_1vE-308z7lRhmlY4FqMqh_E_69NJV60ezIKHUBt-wnyUV_bCIRyS7jsjqSc75hg/exec"
+};
